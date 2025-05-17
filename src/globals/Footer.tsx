14f37fd36b34899054fd2890080946e8dc8b6dac
protamim <s163.tamim@gmail.com>
2025-05-17 19:50:14 +0600

    fix: update URL for link in FooterNavigation component

--- src/globals/Footer.tsx
+++ src/globals/Footer.tsx
@@ -129,7 +129,7 @@ export const FooterNavigation = () => {
         },
         {
           label: "Our Teachers",
-          url: "#",
+          url: "/our-teachers",
         },
         {
           label: "Testimonials",
